d0d3f7f4c1ad650a8baaf605911fe0b7775a3eb7
ozoiiz <129177234+ozoiiz@users.noreply.github.com>
2024-10-07 22:17:03 +0900

    메인 메뉴 및 정렬 기능 추가

--- StudentManager.cpp
+++ StudentManager.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 
 #define STUDENT_LIST_SIZE 100
 
@@ -11,12 +12,12 @@ using namespace std;
 StudentManager::StudentManager(string filepath) :student_count(0) {
     fstream repo;
     repo.open(filepath, ios::in);
-    if (!repo.is_open()) { // 파일 존재 x
+    if (!repo.is_open()) {
         cout<< "File does not exist. Attempting to create new ..." << endl;
         repo.open(filepath, ios::out);
         if (!repo.is_open()) {
             cerr << "Unable to create file. Check permissions." << endl;
-        } else { // 파일 생성 성공
+        } else {
             cout<< "New file " << filepath << " created." << endl; 
             this->filepath = filepath;
             repo.close();
@@ -27,8 +28,7 @@ StudentManager::StudentManager(string filepath) :student_count(0) {
         student_list = new Student[STUDENT_LIST_SIZE]; 
 
         repo.seekg(0, ios::beg);
-        while(getline(repo, line)) { 
-            // 파싱 (이름,학번,출생연도,학과명,전화번호)
+        while(getline(repo, line)) {
             stringstream ss(line);
             string name, dept, tel;
             int id, birth_year;
@@ -50,10 +50,10 @@ StudentManager::StudentManager(string filepath) :student_count(0) {
 }
 
 void StudentManager::addStudent() {
-    string name; //up to 15 chars
-    int id; //exactly 10 digits
-    int birth_year; // exactly 4 digits
-    string department; // may contain space
+    string name;
+    int id;
+    int birth_year;
+    string department;
     string tel;
 
     while(true) {
@@ -64,7 +64,7 @@ void StudentManager::addStudent() {
         else 
             break;
     }
-    
+
     while(true) {
         cout << "Student ID: ";
         cin >> id;
@@ -90,7 +90,7 @@ void StudentManager::addStudent() {
     while(true) {
         cout << "Tel: ";
         cin >> tel;
-        if (name.length() > 12) 
+        if (tel.length() > 12) 
             cerr << "Tel. cannot exceed 12 characters.";
         else 
             break;
@@ -111,7 +111,7 @@ void StudentManager::addStudent() {
     cout << "Successfully added new student." << endl;
 }
 
-void StudentManager::printAll() { // 요구사항 형식에 맞게 추후 수정
+void StudentManager::printAll() {
     for(int i=0; i<student_count; i++) {
         student_list[i].print();
     }
@@ -119,24 +119,100 @@ void StudentManager::printAll() { // 요구사항 형식에 맞게 추후 수정
 
 void StudentManager::displayMenu() {
     while(true) {
+        cout << "\n- Main Menu" << endl;
         cout << "1. Insertion" << endl;
         cout << "2. Search" << endl;
         cout << "3. Sorting Option" << endl;
         cout << "4. Exit" << endl;
-        cout << ">> ";
-        char choice = getchar();
+        cout << "> ";
+        char choice;
+        cin >> choice;
+        cin.ignore();
 
         switch (choice) {
-            case 1:
+            case '1':
                 addStudent();
-                continue;
-            case 4:
                 break;
+            case '2':
+                cout << "Search function not implemented yet." << endl;
+                break;
+            case '3':
+                displaySortingMenu();
+                break;
+            case '4':
+                cout << "Exiting program." << endl;
+                return;
             default:
-                continue;
+                cout << "Invalid option. Please try again." << endl;
+                break;
         }
-        while (getchar() != '\n');
     }
 }
 
-StudentManager::~StudentManager() {};
+void StudentManager::displaySortingMenu() {
+    cout << "\n- Sorting Options" << endl;
+    cout << "1. Sort by Name" << endl;
+    cout << "2. Sort by Student ID" << endl;
+    cout << "3. Sort by Birth Year" << endl;
+    cout << "4. Sort by Department name" << endl;
+    cout << "> ";
+
+    char choice;
+    cin >> choice;
+    cin.ignore();
+
+    switch (choice) {
+        case '1':
+            sortByName();
+            break;
+        case '2':
+            sortById();
+            break;
+        case '3':
+            sortByBirthYear();
+            break;
+        case '4':
+            sortByDepartment();
+            break;
+        default:
+            cout << "Invalid option. Returning to main menu." << endl;
+            return;
+    }
+    cout << "Sorting complete. Returning to main menu." << endl;
+}
+
+void StudentManager::sortById() {
+    sort(student_list, student_list + student_count, [](const Student& a, const Student& b) {
+        return a.getId() < b.getId();
+    });
+    cout << "Sorted by Student ID." << endl;
+    printAll();
+}
+
+void StudentManager::sortByName() {
+    sort(student_list, student_list + student_count, [](const Student& a, const Student& b) {
+        return a.getName() < b.getName();
+    });
+    cout << "Sorted by Name." << endl;
+    printAll();
+}
+
+void StudentManager::sortByBirthYear() {
+    sort(student_list, student_list + student_count, [](const Student& a, const Student& b) {
+        return a.getBirthYear() < b.getBirthYear();
+    });
+    cout << "Sorted by Birth Year." << endl;
+    printAll();
+}
+
+void StudentManager::sortByDepartment() {
+    sort(student_list, student_list + student_count, [](const Student& a, const Student& b) {
+        return a.getDepartment() < b.getDepartment();
+    });
+    cout << "Sorted by Department." << endl;
+    printAll();
+}
+
+StudentManager::~StudentManager() {
+    delete[] student_list;
+}
