f2e30c059a56ca396614be2d5951d705d3f18f6f
dylan <nyskyline7@gmail.com>
2024-10-08 21:53:52 +0900

    search 기능 통합

--- StudentManager.cpp
+++ StudentManager.cpp
@@ -127,9 +127,12 @@ void StudentManager::displayMenu() {
         char choice = getchar();
 
         switch (choice) {
-            case 1:
+            case '1':
                 addStudent();
-                continue;
+                break;
+            case '2':
+                displaySearchMenu();
+                break;
             case 4:
                 break;
             default:
@@ -143,24 +146,63 @@ void StudentManager::displayMenu() {
 StudentManager::~StudentManager() {};
 
 void StudentManager::displaySearchMenu() {
-    std::cout << "- Search -" << std::endl;
-    std::cout << "1. Search by name" << std::endl;
-    std::cout << "2. Search by student ID (10 digits)" << std::endl;
-    std::cout << "3. Search by admission year (4 digits)" << std::endl;
-    std::cout << "4. Search by birth year (4 digits)" << std::endl;
-    std::cout << "5. Search by department name" << std::endl;
-    std::cout << "6. List All" << std::endl;
-    std::cout << "> ";
+    while (true) {
+        int choice;
+        string input;
+        cout << "- Search -" << endl;
+        cout << "1. Search by name" << endl;
+        cout << "2. Search by student ID (10 digits)" << endl;
+        cout << "3. Search by admission year (4 digits)" << endl;
+        cout << "4. Search by birth year (4 digits)" << endl;
+        cout << "5. Search by department name" << endl;
+        cout << "6. List All" << endl;
+        cout << "> ";
+        
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter student name: ";
+                cin >> input;
+                searchByName(input);  // Call StudentManager's searchByName
+                break;
+            case 2:
+                cout << "Enter student ID: ";
+                cin >> input;
+                searchByStudentID(input);  // Call StudentManager's searchByStudentID
+                break;
+            case 3:
+                cout << "Enter admission year: ";
+                cin >> input;
+                searchByAdmissionYear(input);  // Call StudentManager's searchByAdmissionYear
+                break;
+            case 4:
+                cout << "Enter birth year: ";
+                cin >> input;
+                searchByBirthYear(input);  // Call StudentManager's searchByBirthYear
+                break;
+            case 5:
+                cout << "Department name keyword? ";
+                cin >> input;
+                searchByDepartment(input);  // Call StudentManager's searchByDepartment
+                break;
+            case 6:
+                listAll();  // Call StudentManager's listAll
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    }
 }
 
 
-void StudentManager::searchByName(const std::string& name) {
+void StudentManager::searchByName(const string& name) {
     bool found = false;
     Student* current = student_list;  // Assuming student_list is the head pointer of the linked list
     
     while (current != nullptr) {
         if (current->getName() == name) {
-            current->display();
+            current->print();
             found = true;
             break;  // If you want to stop after finding the first match
         }
@@ -168,13 +210,13 @@ void StudentManager::searchByName(const std::string& name) {
     }
 
     if (!found) {
-        std::cout << "No student found with name: " << name << std::endl;
+        cout << "No student found with name: " << name << endl;
     }
 }
 
 
-void StudentManager::searchByStudentID(const std::string& id) {
-    int idcopy = std::stoi(id);
+void StudentManager::searchByStudentID(const string& id) {
+    int idcopy = stoi(id);
     bool found = false;
     Student* current = student_list;
 
@@ -188,20 +230,20 @@ void StudentManager::searchByStudentID(const std::string& id) {
     }
 
     if (!found) {
-        std::cout << "No student found with ID: " << id << std::endl;
+        cout << "No student found with ID: " << id << endl;
     }
 }
 
 
-void StudentManager::searchByAdmissionYear(const std::string& year) {
+void StudentManager::searchByAdmissionYear(const string& year) {
     bool found = false;
     Student* current = student_list;
 
     for (int i = 0; i < STUDENT_LIST_SIZE; ++i) {
-        std::stringstream ss;
+        stringstream ss;
         ss << current->getId();
-        std::string idStr = ss.str();
-        std::string idFirstFour = idStr.substr(0, 4);
+        string idStr = ss.str();
+        string idFirstFour = idStr.substr(0, 4);
 
         if (idFirstFour == year) {
             current->display();
@@ -211,13 +253,13 @@ void StudentManager::searchByAdmissionYear(const std::string& year) {
     }
 
     if (!found) {
-        std::cout << "No student found admitted in year: " << year << std::endl;
+        cout << "No student found admitted in year: " << year << endl;
     }
 }
 
 
-void StudentManager::searchByBirthYear(const std::string& year) {
-    int yearcopy = std::stoi(year);
+void StudentManager::searchByBirthYear(const string& year) {
+    int yearcopy = stoi(year);
     bool found = false;
 
     for (int i = 0; i < STUDENT_LIST_SIZE; ++i) {
@@ -228,26 +270,26 @@ void StudentManager::searchByBirthYear(const std::string& year) {
     }
 
     if (!found) {
-        std::cout << "No student found born in year: " << year << std::endl;
+        cout << "No student found born in year: " << year << endl;
     }
 }
 
-void StudentManager::searchByDepartment(const std::string& department) {
+void StudentManager::searchByDepartment(const string& department) {
     bool found = false;
 
     for (int i = 0; i < STUDENT_LIST_SIZE; ++i) {
-        if (student_list[i].getDepartment().find(department) != std::string::npos) {
+        if (student_list[i].getDepartment().find(department) != string::npos) {
             student_list[i].display();
             found = true;
         }
     }
 
     if (!found) {
-        std::cout << "No student found in department: " << department << std::endl;
+        cout << "No student found in department: " << department << endl;
     }
 }
 
 
 void StudentManager::listAll(){
-    std::cout << "not made yet";
+    cout << "not made yet";
 }
--- main.cpp
+++ main.cpp
@@ -12,52 +12,7 @@ int main(int argc, char* argv[]) {
     string filepath = argv[1];
     StudentManager sm = StudentManager(filepath);
 
-    sm.printAll();
-    sm.addStudent();
-    sm.printAll();
-
-    //searching(황병윤)
-    sm.displaySearchMenu();
+    sm.displayMenu();
     
-    int choice;
-    std::string input;
-
-    while (true) {
-        
-        std::cin >> choice;
-
-        switch (choice) {
-            case 1:
-                std::cout << "Enter student name: ";
-                std::cin >> input;
-                sm.searchByName(input);  // Call StudentManager's searchByName
-                break;
-            case 2:
-                std::cout << "Enter student ID: ";
-                std::cin >> input;
-                sm.searchByStudentID(input);  // Call StudentManager's searchByStudentID
-                break;
-            case 3:
-                std::cout << "Enter admission year: ";
-                std::cin >> input;
-                sm.searchByAdmissionYear(input);  // Call StudentManager's searchByAdmissionYear
-                break;
-            case 4:
-                std::cout << "Enter birth year: ";
-                std::cin >> input;
-                sm.searchByBirthYear(input);  // Call StudentManager's searchByBirthYear
-                break;
-            case 5:
-                std::cout << "Department name keyword? ";
-                std::cin >> input;
-                sm.searchByDepartment(input);  // Call StudentManager's searchByDepartment
-                break;
-            case 6:
-                sm.listAll();  // Call StudentManager's listAll
-                break;
-            default:
-                std::cout << "Invalid choice. Please try again." << std::endl;
-        }
-    }
     return 0;
 }
